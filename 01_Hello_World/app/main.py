c22557f1a8653116c8985c84ce8a283cbe6d16db
James <jamesbrendamour7@yahoo.com>
2024-12-04 03:44:36 -0700

    Add HTML index page to FastAPI app
    Implemented a new HTML index page displayed at the root URL using FastAPI. Updated the existing endpoint to return HTML content instead of a JSON response. This enhances the user interface for the FastAPI crash course by providing a styled web page.

--- 01_Hello_World/app/main.py
+++ 01_Hello_World/app/main.py
@@ -1,18 +1,24 @@
 from fastapi import FastAPI, APIRouter
-
+from starlette.responses import HTMLResponse
 
 app = FastAPI(title="Recipe API", openapi_url="/openapi.json")
 
 api_router = APIRouter()
 
 
-@api_router.get("/", status_code=200)
-def root() -> dict:
-    """
-    Root GET
-    """
-    return {"msg": "Hello, World!"}
+# @api_router.get("/", status_code=200)
+# def root() -> dict:
+#     """
+#     Root GET
+#     """
+#     return {"msg": "Hello, World!"}
 
+@app.get("/", response_class=HTMLResponse)
+async def root():
+    # display index.html
+    print("Starting")
+    with open("index.html", "r") as f:
+        return HTMLResponse(content=f.read(), status_code=200)
 
 app.include_router(api_router)
 
