f7604d0e5fbace495a55da18e3ecefa903e5f6f2
James <jamesbrendamour7@yahoo.com>
2024-12-04 04:09:06 -0700

    Enable autoreload in debugging configuration.
    Updated the `uvicorn.run` calls to include the `reload=True` argument, allowing the server to automatically reload during development when code changes are detected. This change improves the development workflow by reducing the manual effort needed to restart the server after code modifications.

--- 02-Path_Params/app/main.py
+++ 02-Path_Params/app/main.py
@@ -88,8 +88,7 @@ async def create_video(video: Video):
 # main.py
 # Created/Modified files during execution:
 # main.py
-# if __name__ == "__main__":
-#     # Use this for debugging purposes only
-#     import uvicorn
-#
-#     uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
+if __name__ == "__main__":
+    # Use this for debugging purposes only
+    import uvicorn
+    uvicorn.run('main:app', host="0.0.0.0", port=8001, log_level="debug", reload=True)
